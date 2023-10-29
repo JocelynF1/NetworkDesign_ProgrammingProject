e6e61d528b28c1ac41ac41283e35fedfc7031f75
Michael-Burton-2 <144968458+Michael-Burton-2@users.noreply.github.com>
2023-10-28 21:47:41 -0400

    Working code with acks and sequence numbers and placeholder checksum. Still need to integrate corruptor for acks and data.

--- UDPClient.py
+++ UDPClient.py
@@ -2,8 +2,7 @@
 # UDPClient.py
 # Phase 2, EECE 5830, Fall 2023
 # 1 October 2023
-
-
+import struct
 from socket import *
 import hashlib
 import random
@@ -32,7 +31,7 @@ DATA_OFFSET = ACK_SEQ_SIZE + CHECKSUM_SIZE
 # Input: file_name: string representing the path to the file to read from
 # Input: packet_size: integer representing the number of Bytes in a packet
 # Output: packet_list: a list of Bytes objects to be sent to the server
-def Make_Packet(file_name, packet_size):
+def Make_Packet(file_name, data_size):
     # If the .bmp file is not in the same directory as the code, the full path should be prepended to the file name
     # e.g., path = "C:/Users/Michael Burton/Documents/UML/Fall2023/Network/Project/",
     # file_name = path + file_name
@@ -40,19 +39,74 @@ def Make_Packet(file_name, packet_size):
 
     packet_list = []
 
-    # first packet is the number of packets being sent
-    packet = f.read(packet_size)
+    # first packet is the number of packets being sent, this is setting up the second packet sent, so SEQ_1 is used
+    seq_to_send = SEQ_1
+    packet = bytearray()
+
+    data = f.read(data_size)
+
+    # cs_packet is the proto-header (missing checksum) and the data for creating checksum
+    cs_packet = bytearray()
+    cs_packet.append(ACK)
+    cs_packet.append(seq_to_send)
+    cs_packet.extend(data)
+    csum = checksum(cs_packet)
+
+    packet.extend(csum)
+    packet.append(ACK)
+    packet.append(seq_to_send)
+    packet.extend(data)
+
     packet_list.append(packet)
 
-
     seq_to_send = SEQ_0
-
     # While there is still data left in the file, read another packet.
     # On the last packet, it will be smaller than the packet_size specified, so we know we are done
-    while len(packet) == packet_size:
-        data = f.read(packet_size)
+    while len(data) == data_size:
+        data = f.read(data_size)
 
+        packet = bytearray()
+
+        # cs_packet is the proto-header (missing checksum) and the data for creating checksum
+        cs_packet = bytearray()
+        cs_packet.append(ACK)
+        cs_packet.append(seq_to_send)
+        cs_packet.extend(data)
+
+        csum = checksum(cs_packet)
+
+        packet.extend(csum)
+        packet.append(ACK)
+        packet.append(seq_to_send)
+        packet.extend(data)
+        print(packet)
         packet_list.append(packet)
+
+        if seq_to_send == SEQ_1:
+            seq_to_send = SEQ_0
+        else:
+            seq_to_send = SEQ_1
+
+    data = struct.pack(">I", len(packet_list))
+
+    packet = bytearray()
+
+    cs_packet = bytearray()
+    cs_packet.append(ACK)
+    cs_packet.append(SEQ_0)
+    cs_packet.extend(data)
+    csum = checksum(cs_packet)
+
+    packet.extend(csum)
+    packet.append(ACK)
+    packet.append(SEQ_0)
+    packet.extend(data)
+
+    # print(packet)
+    # print(packet_list[0])
+
+    # print(split_packet(packet))
+    packet_list.insert(0, packet)
 
     print("Packets Made. Number of packets: ", len(packet_list), "file position: ", f.tell())
     print("First packet: ", packet_list[0])
@@ -74,25 +128,29 @@ def corrupt_ack(ack, seq):
     return corrupt_ack, corrupt_seq
 
 
-def checksum(ack_response, seq_response, data):
+def checksum(message):
     # Will put custom hashing function later, this is temporary for testing purposes
-    cs_message = bytearray()
-    cs_message.append(ack_response)
-    cs_message.append(seq_response)
-    cs_message.extend(data)
     h = hashlib.blake2b(digest_size=CHECKSUM_SIZE)
-    h.update(cs_message)
-    return h.digest()
+    h.update(message)
+    return bytearray.fromhex(h.hexdigest())
 
 
 def split_packet(message):
-    print(len(message))
-    checksum = message[0:CHECKSUM_SIZE]
+    print(message)
+    csum = message[0:CHECKSUM_SIZE]
     ack_response = message[CHECKSUM_SIZE]
     seq_response = message[CHECKSUM_SIZE + 1]
     data = message[DATA_OFFSET:]
 
-    return checksum, ack_response, seq_response, data
+    return csum, ack_response, seq_response, data
+
+
+def split_ack_packet(received_msg):
+    csum = received_msg[0:CHECKSUM_SIZE]
+    ack_response = received_msg[CHECKSUM_SIZE]
+    seq_response = received_msg[CHECKSUM_SIZE + 1]
+
+    return csum, ack_response, seq_response
 
 
 def is_ack(ack_received, seq_received, expected_seq_num):
@@ -137,21 +195,30 @@ class UDPClient:
             self.send(message)
             return S_Wait_for_ACK_0
         elif self.state == S_Wait_for_ACK_0:
-            msg = self.receive(bdata_size)
-            checksum, ack_response, seq_response, data = split_packet(msg)
-            new_checksum = checksum(ack_response, seq_response, data)
-            if not is_corrupt(checksum, new_checksum) and is_ack(ack_response, seq_response, SEQ_0):
+            received_msg = self.receive(bdata_size)
+            csum, ack_response, seq_response = split_ack_packet(received_msg)
+            cs_packet = bytearray()
+            cs_packet.append(ack_response)
+            cs_packet.append(seq_response)
+            new_checksum = checksum(cs_packet)
+            print("Inputs to New checksum Client side ACK_0: ", received_msg)
+            print("New checksum Client side ACK_0: ", new_checksum)
+            if not is_corrupt(csum, new_checksum) and is_ack(ack_response, seq_response, SEQ_0):
                 return S_Wait_for_call_1_from_above
             else:
+                self.send(message)
                 return S_Wait_for_ACK_0
         elif self.state == S_Wait_for_call_1_from_above:
             self.send(message)
             return S_Wait_for_ACK_1
         elif self.state == S_Wait_for_ACK_1:
-            msg = self.receive(bdata_size)
-            checksum, ack_response, seq_response, data = split_packet(msg)
-            new_checksum = checksum(ack_response, seq_response, data)
-            if not is_corrupt(checksum, new_checksum) and is_ack(ack_response, seq_response, SEQ_1):
+            received_msg = self.receive(bdata_size)
+            csum, ack_response, seq_response = split_ack_packet(received_msg)
+            cs_packet = bytearray()
+            cs_packet.append(ack_response)
+            cs_packet.append(seq_response)
+            new_checksum = checksum(cs_packet)
+            if not is_corrupt(csum, new_checksum) and is_ack(ack_response, seq_response, SEQ_1):
                 return S_Wait_for_call_0_from_above
             else:
                 self.send(message)
@@ -170,26 +237,11 @@ if __name__ == '__main__':
 
     client = UDPClient(name_receiver, port_receiver)
 
-    # Sends packets to the server with a message packet size as 2048 Bytes
-    packets = Make_Packet("LAND2.BMP", 2048)
+    # Sends packets to the server with a message data size as 2048 Bytes
+    packets = Make_Packet("LAND2.BMP", 2044)
 
     # Test case for when the file size = 0
     # packets = Make_Packet("unknown.BMP", 2048)
-
-    # packet_length gets the length of the packet array, and converts it to a string
-    packet_length = str(len(packets))
-
-    # message is converted from a string to a bytes object, and then sent to the server as the message that
-    # client is sending packets
-    client.send(packet_length.encode())
-
-    # for each packet in the list packets, send them to the client
-    for x in packets:
-        client.send(x)
-
-    # Tell the server we are f
-    # Tell the server we are f
-    client.send(packet_length.encode())
 
     packet_index = 0
 
@@ -198,6 +250,6 @@ if __name__ == '__main__':
     while packet_index < len(packets):
         sender_state = client.next_state(packets[packet_index], 2048)
         if sender_state == S_Wait_for_call_0_from_above or sender_state == S_Wait_for_call_1_from_above:
-            packet_index += 1
+            packet_index += 1  # we can advance the index since the packet was sent properly
 
         client.state = sender_state  # advance the state
--- UDPServer.py
+++ UDPServer.py
@@ -3,7 +3,7 @@
 # Phase 2, EECE 5830, Fall 2023
 # 1 October 2023
 
-
+import struct
 from socket import *
 import hashlib
 import random
@@ -22,6 +22,8 @@ ACK = 255  # need to define the ack, should be bytes object with ACK and correct
 SEQ_0 = 0
 SEQ_1 = 255
 ACK_SEQ_SIZE = 2
+
+#PROPER_CHECKSUM = bytearray(b'\x00\x00')  # temporary value to use until checksum implemented
 
 DATA_OFFSET = ACK_SEQ_SIZE + CHECKSUM_SIZE
 
@@ -61,15 +63,15 @@ def checksum(message):
     # Will put custom hashing function later, this is temporary for testing purposes
     h = hashlib.blake2b(digest_size=CHECKSUM_SIZE)
     h.update(message)
-    return h.digest()
+    return bytearray.fromhex(h.hexdigest())
 
 
 def split_packet(message):
-    print(len(message))
-    checksum = message[0:CHECKSUM_SIZE]
-    ack_response = message[CHECKSUM_SIZE]
-    seq_response = message[CHECKSUM_SIZE + 1]
-    data = message[DATA_OFFSET:]
+    copy_msg = bytearray(message)
+    checksum = copy_msg[0:CHECKSUM_SIZE]
+    ack_response = copy_msg[CHECKSUM_SIZE]
+    seq_response = copy_msg[CHECKSUM_SIZE + 1]
+    data = copy_msg[DATA_OFFSET:]
 
     return checksum, ack_response, seq_response, data
 
@@ -96,100 +98,98 @@ class UDPServer:
         # UDP = User Datagram Protocol
         # Windows socket API: https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-socket
         # By default, the socket object is in blocking mode
-
+        self.data_buffer = []
         print("Server initialized")
 
+    def send(self, message):
+        self.socket.sendto(message, (self.name_receiver, self.port_receiver))
+        # client sends message (converted to Bytes) to server
+
     # receive
-    # description: receives data from the client
-    # Input: bdata_size, buffer size in bytes
-    # Output: Message to Console confirming that the server is ready to receive
-    # Output: test data, such as number of packets received, the list of packets,
-    # and then the length of the list of packets to make sure it matched the number of packets received
-    # Output: received_packets, the list of Bytes objects containing packet messages
-
+    # input: bdata_size
     def receive(self, bdata_size):
+        message, server_address = self.socket.recvfrom(bdata_size)
 
-        self.socket.bind(('', self.port_receiver))  # bind binds the socket to a particular port to listen on
-        # The argument is a tuple in the form (,) , where the first part of the tuple corresponds to
-        # the host address ('' represents INADDR_ANY, which binds to any local interface)
-        # and the second part is the port to listen on
-        # Linked python socket documentation for this piece near the import statement
-
-        print("Server \"", self.name_receiver, "\" is ready on port \"", self.port_receiver, "\"")
-
-        message = None
-        while message is None:
-            message, client_address = self.socket.recvfrom(bdata_size+DATA_OFFSET)
-
-        num_packets_encoded = message
-
-        num_packets = int(message.decode())
-
-        print(num_packets)
-        received_packets = []
-
-        message = bytes()
-
-        while message != num_packets_encoded:
-            message, client_address = self.socket.recvfrom(bdata_size)
-            received_packets.append(message)
-        received_packets.pop()
-
-        # Q: What happens if the number of packets is zero?
-        # A: client only sends one packet, an empty string of bytes (b'')
-        print(received_packets)
-        print(len(received_packets))
-
-        return received_packets
+        return message, server_address
 
     def next_state(self, bdata_size):
         if self.state == S_Wait_for_0_from_below:
-            msg, client_address = self.socket.recvfrom(bdata_size)
-            checksum, ack_response, seq_response, data = split_packet(msg)
+            msg, client_address = self.receive(bdata_size)
+            csum, ack_response, seq_response, data = split_packet(msg)
+
             cs_message = bytearray()
             cs_message.append(ack_response)
             cs_message.append(seq_response)
             cs_message.extend(data)
             new_checksum = checksum(cs_message)
 
+            # ack message to send back
             ack_msg = bytearray()
-            ack_msg.append(checksum)
-            ack_msg.append(ack_response)
-            if not is_corrupt(checksum, new_checksum) and is_ack(ack_response, seq_response, SEQ_0):
+
+            # everything except the checksum field is passed to the checksum function
+            ack_cs_msg = bytearray()
+            ack_cs_msg.append(ACK)
+
+            if not is_corrupt(csum, new_checksum) and is_ack(ack_response, seq_response, SEQ_0):
                 # deliver data
+                self.data_buffer.append(data)
                 # send pos ack
-                ack_msg.append(SEQ_0)
+                ack_cs_msg.append(SEQ_0)
+                ack_cs = checksum(ack_cs_msg)
+
+                ack_msg.extend(ack_cs)
+                ack_msg.extend(ack_cs_msg)
+
                 self.socket.sendto(ack_msg, client_address)
                 return S_Wait_for_1_from_below
             else:
-                ack_msg.append(SEQ_1)
+                ack_cs_msg.append(SEQ_1)
+                ack_cs = checksum(ack_cs_msg)
+
+                ack_msg.extend(ack_cs)
+                ack_msg.extend(ack_cs_msg)
                 self.socket.sendto(ack_msg, client_address)
                 return S_Wait_for_0_from_below
 
         elif self.state == S_Wait_for_1_from_below:
-            msg, client_address = self.socket.recvfrom(bdata_size)
-            checksum, ack_response, seq_response, data = split_packet(msg)
+            msg, client_address = self.receive(bdata_size)
+            csum, ack_response, seq_response, data = split_packet(msg)
+
             cs_message = bytearray()
             cs_message.append(ack_response)
             cs_message.append(seq_response)
             cs_message.extend(data)
             new_checksum = checksum(cs_message)
 
+            # ack message to send back
             ack_msg = bytearray()
-            ack_msg.append(checksum)
-            ack_msg.append(ack_response)
-            if not is_corrupt(checksum, new_checksum) and is_ack(ack_response, seq_response, SEQ_1):
+
+            # everything except the checksum field is passed to the checksum function
+            ack_cs_msg = bytearray()
+            ack_cs_msg.append(ACK)
+
+            if not is_corrupt(csum, new_checksum) and is_ack(ack_response, seq_response, SEQ_1):
                 # deliver data
+                self.data_buffer.append(data)
                 # send pos ack
-                ack_msg.append(SEQ_1)
+                ack_cs_msg.append(SEQ_1)
+                ack_cs = checksum(ack_cs_msg)
+
+                ack_msg.extend(ack_cs)
+                ack_msg.extend(ack_cs_msg)
+
                 self.socket.sendto(ack_msg, client_address)
                 return S_Wait_for_0_from_below
             else:
-                ack_msg.append(SEQ_0)
+                ack_cs_msg.append(SEQ_0)
+                ack_cs = checksum(ack_cs_msg)
+
+                ack_msg.extend(ack_cs)
+                ack_msg.extend(ack_cs_msg)
                 self.socket.sendto(ack_msg, client_address)
                 return S_Wait_for_1_from_below
         else:
-            return 10
+            return 10  # error
 
 
 # Run the program from here
@@ -200,11 +200,27 @@ if __name__ == '__main__':
     # This is the receiver port, so whichever system is designated as a receiver will use this to listen
 
     server = UDPServer(name_receiver, port_receiver)
+    server.socket.bind(('', server.port_receiver))
 
+    buffer_size = 2048
     # Receives packets from client with a message buffer size on each packet as 2048 Bytes
-    received_packets = server.receive(2048)
+
+    receiver_state = server.next_state(buffer_size)
+    server.state = receiver_state
+
+    print("Got Here!")
+    data = server.data_buffer.pop(0)
+    print(data)
+    num_packets = int.from_bytes(data)
+
+    packet_index = 0
+
+    while packet_index < num_packets:
+        receiver_state = server.next_state(buffer_size)
+        if server.state != receiver_state:
+            packet_index += 1
+
+        server.state = receiver_state
 
     # "copy.bmp" is where the data from the packets received get written to, location is local to the code directory
-    deliver_data(received_packets, "copy.bmp")
-
-
+    deliver_data(server.data_buffer, "copy.bmp")
